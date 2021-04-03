d4d043e2bcfe0591523692dbbafacf5cf68efd33
krateng <git.noreply@krateng.ch>
2021-04-04 00:09:47 +0200

    Can now load directly from container config

--- proxymloxy/proxymloxy.py
+++ proxymloxy/proxymloxy.py
@@ -5,16 +5,29 @@ import os
 from jinja2 import Environment, PackageLoader
 from doreah.control import mainfunction
 
+DEFAULT_NGINX_CONF_FILE = "/etc/nginx/conf.d/proxymloxy.conf"
+CONTAINER_CONF_FILE = "/etc/pve/lxc/{cid}.conf"
+
 
 jenv = Environment(
     loader=PackageLoader('proxymloxy','.')
 )
 jtmpl = jenv.get_template("configfile.jinja")
 
+def load_container_ip(cid):
+	print("get container ip of",cid)
+	with open(CONTAINER_CONF_FILE.format(cid=cid)) as fd:
+		lines = fd.readlines()
+	print("Loaded container {cid} configuration")
+	info = dict([entry.split('=') for entry in [l for l in lines if l.startswith('net0: ')][0].split(": ")[-1].split(",")])
+	
+	return ipaddress.IPv4Interface(info['ip']).ip, ipaddress.IPv6Interface(info['ip6']).ip
+
 def load_yml_file(name):
 	try:
 		with open(name,"r") as ymlfile:
 			info = yaml.safe_load(ymlfile)
+		print("Loaded configuration")
 		return info
 	except:
 		print("Input file",name,"could not be found!")
@@ -24,9 +37,10 @@ def write_conf_file(txt,name):
 	#if not os.path.exists(name) or ask("Overwrite " + name):
 	
 	try:
-		os.makedirs(os.path.dirname(name),exist_ok=True)
+		os.makedirs(os.path.dirname(os.path.abspath(name)),exist_ok=True)
 		with open(name,"w") as conffile:
 			conffile.write(txt)
+		print("Wrote nginx config file")
 		return True
 	except:
 		print("Output file",name,"could not be written!")
@@ -49,12 +63,19 @@ def create_conf_file_new(info):
 		
 			server['names'] = [name + "." + domain['domain'] for name in server['names']]
 		
+			
 			if server["ipv6"]:
-				host = int(str(server["host"]),16) - 1
-				server['ip'] = "[" + str(next(islice(ipv6network.hosts(),host,None))) + "]"
+				if "container" in server:
+					server['ip'] = load_container_ip(server["container"])[1]
+				else:
+					host = int(str(server["host"]),16) - 1
+					server['ip'] = "[" + str(next(islice(ipv6network.hosts(),host,None))) + "]"
 			else:
-				host = int(server["host"]) - 1
-				server['ip'] = str(next(islice(ipv4network.hosts(),host,None)))
+				if "container" in server:
+					server['ip'] = load_container_ip(server["container"])[0]
+				else:
+					host = int(server["host"]) - 1
+					server['ip'] = str(next(islice(ipv4network.hosts(),host,None)))
 				
 	return jtmpl.render(**info)
 	
@@ -69,8 +90,10 @@ def translate(input,output):
 	
 
 def restart_nginx():
+	print("Restarting Nginx")
 	os.system("sudo systemctl restart nginx.service")
+	print("Success!")
 	
 @mainfunction({'i':'input','o':'output'},shield=True)
-def main(input="proxymloxy.yml",output="/etc/nginx/conf.d/proxymloxy.conf"):
+def main(input="proxymloxy.yml",output=DEFAULT_NGINX_CONF_FILE):
 	if translate(input,output): restart_nginx()
